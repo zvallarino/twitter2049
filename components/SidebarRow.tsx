1ea530025b31ab1085c4e8191a3bcdff558f82f4
zachary vallarino <zvallarino@gmail.com>
2022-06-01 21:17:42 -0400

    basically complete

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -3,11 +3,12 @@ import React, { SVGProps } from 'react'
 interface Props {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
   title:string
+  onClick?: () => {}
 }
 
-function SidebarRow({Icon, title}: Props) {
+function SidebarRow({Icon, title, onClick}: Props) {
   return (
-    <div className="flex max-w-fit items-centers space-x-2 px-4 py-3
+    <div onClick ={()=> onClick?.()}className="flex max-w-fit items-centers space-x-2 px-4 py-3
     rounded-full cursor:pointer 
     transition-all duration-200 hover:bg-gray-100 group
     ">
